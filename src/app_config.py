b313bc35ed987c43f3b2cb46515c04164ac63a2e
Ankit Kumar <akumar78210@gmail.com>
2025-07-19 17:25:03 +0530

    Update app_config.py

--- src/app_config.py
+++ src/app_config.py
@@ -1,8 +1,8 @@
 # src/app_config.py
 config = {
-    "openai_api_key": "sk_907dda2a5ae5653c63278c4418d845c91e4cedf171772f78d906cfad2e2f4db4",
+    "openai_api_key": "your key",
      "sqlite_path": "os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'db', 'movies.sqlite'))",
-    "neo4j_uri": "neo4j://localhost:7687",
-    "neo4j_username": "neo4j",
-    "neo4j_password": "projectdatabase",  # Replace with your actual password
+    "neo4j_uri": "url",
+    "neo4j_username": "username",
+    "neo4j_password": "password",  # Replace with your actual password
 }
